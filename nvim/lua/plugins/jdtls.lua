f87c9e78575a7d729d8a4a51c843e8234131d4ff
WillTheBigCodeMan <will.lucas.evans@outlook.com>
2025-03-20 15:27:50 +0000

    jdtls config

--- nvim/lua/plugins/jdtls.lua
+++ nvim/lua/plugins/jdtls.lua
@@ -1,9 +1,31 @@
-local on_attach = function(client, bufnr)
-    client.server_capabilities.semanticTokensProvider = nil
-end
+local config = {
+    cmd = {
+        "java",
 
-require("lspconfig").jdtls.setup({
-    -- on_attach = on_attach,
+        "-Declipse.application=org.eclipse.jdt.ls.core.id1",
+        "-Dosgi.bundles.defaultStartLevel=4",
+        "-Declipse.product=org.eclipse.jdt.ls.core.product",
+        "-Dlog.protocol=true",
+        "-Dlog.level=ALL",
+        "-Xmx1g",
+        "--add-modules=ALL-SYSTEM",
+        "--add-opens",
+        "java.base/java.util=ALL-UNNAMED",
+        "--add-opens",
+        "java.base/java.lang=ALL-UNNAMED",
+
+        "-jar",
+        "/home/Will/.local/share/nvim/mason/packages/jdtls/plugins/org.eclipse.equinox.launcher_1.6.900.v20240613-2009.jar",
+
+        "-configuration",
+        "/home/Will/.local/share/nvim/mason/packages/jdtls/config_linux/",
+
+        "-data",
+        vim.fn.getcwd(),
+    },
+
+    root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
+
     settings = {
         java = {
             project = {
@@ -13,4 +35,9 @@ require("lspconfig").jdtls.setup({
             },
         },
     },
-})
+}
+require("jdtls").start_or_attach(config)
+
+-- local on_attach = function(client, bufnr)
+--     client.server_capabilities.semanticTokensProvider = nil
+-- end
