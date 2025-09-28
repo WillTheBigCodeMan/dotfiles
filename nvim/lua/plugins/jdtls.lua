826eaa7e15e51fee52564867f879b69ceb7a16b7
WillTheBigCodeMan <will.lucas.evans@outlook.com>
2025-09-28 21:58:38 +0100

    jdtls nvim config update

--- nvim/lua/plugins/jdtls.lua
+++ nvim/lua/plugins/jdtls.lua
@@ -1,12 +1,17 @@
 return {
     "mfussenegger/nvim-jdtls",
     config = {
+        -- root_dir = vim.fs.root(0, { ".git", "mvnw", "gradlew" }),
         settings = {
             java = {
                 project = {
                     referencedLibraries = {
                         "lib/**/*.jar",
                     },
+                    sourcePaths = { "src" },
+                },
+                configuration = {
+                    updateBuildConfiguration = "automatic", -- or "interactive"
                 },
             },
         },
